ef518909758c633af6db66245fbe2e9785fcc326
Alperen Aga <alperencode@gmail.com>
2022-01-12 14:37:49 +0300

    Add Problem43 and Problem7

--- Problem27.py
+++ Problem27.py
@@ -1,4 +1,4 @@
-# 27. Remove Element
+# 27. Remove Element - Easy
 # https://leetcode.com/problems/remove-element/
 class Solution(object):
     def removeElement(self, nums, val):
---
+++ Problem43.py
@@ -0,0 +1,12 @@
+# 43. Multiply Strings - Medium
+class Solution(object):
+    def multiply(self, num1, num2):
+        return str(int(num1) * int(num2))
+
+solution = Solution()
+
+# Testcases
+solution.multiply("2", "3")
+solution.multiply("123", "456")
+
+# Status - Accepted
