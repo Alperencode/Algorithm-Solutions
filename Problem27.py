a3285deacd7fdf87b6588f173a5b7be62b4a6cf1
Alperen Aga <alperencode@gmail.com>
2022-01-12 03:56:45 +0300

    Add Problem27

---
+++ Problem27.py
@@ -0,0 +1,18 @@
+# 27. Remove Element
+# https://leetcode.com/problems/remove-element/
+class Solution(object):
+    def removeElement(self, nums, val):
+        while val in nums:
+            for i in nums:
+                if i == val:
+                    nums.remove(i)
+        k = len(nums)
+        return k
+
+solution = Solution()
+
+# Testcases
+solution.removeElement([3,2,2,3],3)
+solution.removeElement([0,1,2,2,3,0,4,2],2)
+
+# Status - Accepted
