ca82b6e349bd610e3eb683c144959bfe6e1926e9
Alperen Aga <alperencode@gmail.com>
2022-01-18 11:58:25 +0300

    Add Problem1

---
+++ Problem1.py
@@ -0,0 +1,15 @@
+# 1. Two Sum - Easy
+# https://leetcode.com/problems/two-sum/
+class Solution(object):
+    def twoSum(self, nums, target):
+        for index,value in enumerate(nums):
+            for i in range(index + 1,len(nums)):
+                if value + nums[i] == target:
+                    return [index,i]         
+
+# Testcases 
+# [2,7,11,15] , 9
+# [3,2,4] , 6
+# [3,3] , 5
+
+# Status - Accepted
