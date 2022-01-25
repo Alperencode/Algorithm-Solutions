7de35956f8936051b23622512c316ed47e071985
Alperen Aga <alperencode@gmail.com>
2022-01-25 03:06:55 +0300

    Add Problem367

---
+++ Problem367.py
@@ -0,0 +1,12 @@
+# 367. Valid Perfect Square - Easy
+# https://leetcode.com/problems/valid-perfect-square/
+class Solution(object):
+    def isPerfectSquare(self, num):
+        return (num**0.5).is_integer()
+
+# Testcases
+# 16
+# 14
+# 100
+
+# Status - Accepted 
