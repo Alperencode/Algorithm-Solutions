8f733a1add99ace2c58b88018d56035d1349b45e
Alperen Aga <alperencode@gmail.com>
2022-01-26 03:23:12 +0300

    Add Problem551

---
+++ Problem551.py
@@ -0,0 +1,21 @@
+# 551. Student Attendance Record I - Easy
+# https://leetcode.com/problems/student-attendance-record-i/
+class Solution(object):
+    def checkRecord(self, s):
+        aCount = 0
+        for i,v in enumerate(s):
+            if v == "A":
+                aCount += 1
+            if v == "L":
+                if s[i:i+3] == "LLL":
+                    return False
+        if aCount >= 2:
+            return False
+        return True
+
+# Testcases
+# "PPALLP"
+# "PPALLL"
+# "AA"
+
+# Status - Accepted
