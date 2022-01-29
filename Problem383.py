e428801c38d7346ffba60fcb0edaf9ddb68e47fb
Alperen Aga <alperencode@gmail.com>
2022-01-30 00:01:19 +0300

    Add Problem383

---
+++ Problem383.py
@@ -0,0 +1,22 @@
+# 383. Ransom Note - Easy
+# https://leetcode.com/problems/ransom-note/
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        m = []
+        for i in magazine:
+            m.append(i)
+        for i in ransomNote:
+            if i not in m:
+                return False
+            m.remove(i)
+        return True
+
+# Testcases
+# "a"
+# "b"
+# "aa"
+# "ab"
+# "aa"
+# "aab"
+
+# Status - Accepted
