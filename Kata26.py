e3cbb89f8b8359fb71f8a08e0d7d845efcd69188
Alperen Aga <alperencode@gmail.com>
2022-02-01 03:48:06 +0300

    Add Kata26

---
+++ Kata26.py
@@ -0,0 +1,11 @@
+# I love you, a little , a lot, passionately ... not at all
+# https://www.codewars.com/kata/57f24e6a18e9fad8eb000296
+def how_much_i_love_you(nb_petals):
+    return ["I love you","a little","a lot","passionately","madly", "not at all"][nb_petals%6-1]
+
+# Testcases
+# how_much_i_love_you(7) - "I love you"
+# how_much_i_love_you(3) - "a lot"
+# how_much_i_love_you(6) - "not at all"
+
+# Status - Passed - 489ms
