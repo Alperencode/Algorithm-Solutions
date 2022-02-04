b17cb683df0d14fd382e2e3e63ebdceee020713e
Alperen Aga <alperencode@gmail.com>
2022-02-04 06:35:32 +0300

    Add Problem912

---
+++ Problem912.py
@@ -0,0 +1,11 @@
+# 912. Sort an Array - Medium
+# https://leetcode.com/problems/sort-an-array/
+class Solution(object):
+    def sortArray(self, nums):
+        return sorted(nums)
+
+# Testcases
+# [5,2,3,1]
+# [5,1,1,2,0,0]
+
+# Status - Accepted
