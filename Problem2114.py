93544d4c66e56bede54cb18fa580e54a1a9d817c
Alperen Aga <alperencode@gmail.com>
2022-02-04 06:36:01 +0300

    Add Proble2114

---
+++ Problem2114.py
@@ -0,0 +1,15 @@
+# 2114. Maximum Number of Words Found in Sentences - Easy
+# https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+class Solution(object):
+    def mostWordsFound(self, sentences):
+        counter = len(sentences[0].split(" "))
+        for i in sentences:
+            if len(i.split(" ")) >= counter:
+                counter = len(i.split(" "))
+        return counter
+
+# Testcases
+# ["alice and bob love leetcode","i think so too","this is great thanks very much"]
+# ["please wait","continue to fight","continue to win"]
+
+# Status - Accepted
