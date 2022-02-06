49d86932af3029a4ae1f39e3844dcd3f0fb6fd04
Alperen Aga <alperencode@gmail.com>
2022-02-06 05:24:33 +0300

    Add Problem58

---
+++ Problem58.py
@@ -0,0 +1,13 @@
+# 58. Length of Last Word - Easy
+# https://leetcode.com/problems/length-of-last-word/
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        s = s.strip().split(" ")
+        return len(s[-1])
+
+# Testcases
+# "Hello World"
+# "   fly me   to   the moon  "
+# "luffy is still joyboy"
+
+# Status - Accepted
