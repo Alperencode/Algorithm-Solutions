713606b16e818f6f75039e2e1e107af552a6d369
Alperen Aga <alperencode@gmail.com>
2022-02-10 06:09:41 +0300

    Add Problem217

---
+++ Problem217.py
@@ -0,0 +1,17 @@
+# 217. Contains Duplicate - Easy
+# https://leetcode.com/problems/contains-duplicate/
+class Solution(object):
+	def containsDuplicate(self, nums):
+		s = set()
+		for i in nums:
+			if i in s:
+				return True
+			s.add(i)
+		return False
+
+# Testcases
+# [1,2,3,1]
+# [1,2,3,4]
+# [1,1,1,3,3,4,3,2,4,2]
+
+# Status - Accepted
