b626b9f2252e8367a04dcb73ac7c17f9d7a0b8a1
Alperen Aga <alperencode@gmail.com>
2022-02-15 01:47:27 +0300

    Add Kata4

---
+++ Kata4.py
@@ -0,0 +1,19 @@
+# Isograms
+# https://www.codewars.com/kata/54ba84be607a92aa900000f1/python
+def is_isogram(string):
+    d = []
+    for i in string.lower():
+        if i in d:
+            return False
+        d.append(i)
+    return True
+
+# Testcases
+# "Dermatoglyphics" - True
+# "isogram" - True
+# "aba" - False "same chars may not be adjacent"
+# "moOse" - False "same chars may not be same case"
+# "isIsogram" - False
+# "" - True - "an empty string is a valid isogram"
+
+# Status - Passed
