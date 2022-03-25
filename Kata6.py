5fa22c312e65e238f18ad7b0f31f1e5627749cbe
Alperen Aga <alperencode@gmail.com>
2022-03-26 00:22:10 +0300

    Add Kata6

---
+++ Kata6.py
@@ -0,0 +1,20 @@
+def pig_it(text):
+    result = []
+    punctures = ['?','!','.',',',':',';']
+    for i in text.split(' '):
+        if len(i) > 1:
+            result.append(i[1:]+i[0]+'ay')
+        else:
+            if i in punctures:
+                result.append(i)
+            else:
+                result.append(i+'ay')
+    return " ".join(result)
+
+##Note: Learned about isAlpha() method with this kata.
+
+# Testcases
+# 'Pig latin is cool' - 'igPay atinlay siay oolcay'
+# 'This is my string' - 'hisTay siay ymay tringsay'
+
+# Status - Passed
