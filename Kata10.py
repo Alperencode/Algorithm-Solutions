29d0c9a64abf19a471a346261261a13740bd3541
Alperen Aga <alperencode@gmail.com>
2022-03-31 01:25:10 +0300

    Add Kata10

---
+++ Kata10.py
@@ -0,0 +1,21 @@
+# Number of trailing zeros of N!
+# https://www.codewars.com/kata/52f787eb172a8b4ae1000a34/
+def zeros(n):
+    if(n < 0):
+        return -1
+    remainder = 0
+    while(n >= 5):
+        n //= 5
+        remainder += n
+    return remainder
+
+# Testcases
+# 0 - 0
+# 6 - 1
+# 30 - 7
+# 100 - 24
+# 1000 - 249
+# 100000 - 24999
+# 1000000000 - 249999998
+
+# Status - Passed
