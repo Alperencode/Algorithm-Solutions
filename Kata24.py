4c1a2d29881f1086c21bf3834dd8041be29dfb1b
Alperen Aga <alperencode@gmail.com>
2022-05-23 16:13:51 +0300

    Update Kata24

---
+++ Kata24.py
@@ -0,0 +1,14 @@
+# Where my anagrams at?
+# https://www.codewars.com/kata/523a86aa4230ebb5420001e1
+def anagrams(word, words):
+    result = []
+    for item in words:
+        if sorted(word) == sorted(item):
+            result.append(item)
+    return result
+
+# Testcases
+# anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) - ['aabb', 'bbaa']
+# anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) - ['carer', 'racer']
+
+# Status - Passed
