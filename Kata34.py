a9b484dd04acb417f473f911ff3d3874de808cb4
Alperen Aga <alperencode@gmail.com>
2022-06-10 00:16:09 +0300

    Add Kata34

---
+++ Kata34.py
@@ -0,0 +1,23 @@
+# String ends with?
+# https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+def solution(string, ending):
+    return string.endswith(ending)
+
+# Testcases
+# "samurai", "ai" - True
+# "sumo", "omo" - False
+# "ninja", "ja" - True
+# "sensei", "i" - True
+# "samurai", "ra" - False
+# "abc", "abcd" - False
+# "abc", "abc" - True
+# "abcabc", "bc" - True
+# 'ails', 'fails' - False
+# 'fails', 'ails' -  True
+# 'this', 'fails' - False
+# 'abc', '' - True
+# ':-)', ':-(' - False
+# '!@#$%^&*() :-)', ':-)' - True
+# 'abc\n', 'abc' - False
+
+# Status - Passed
