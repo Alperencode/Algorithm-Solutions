cec8a3caee778ae637c918d56dbd3b455e085c15
Alperen Aga <alperencode@gmail.com>
2022-06-11 00:21:35 +0300

    Add Kata35

---
+++ Kata35.py
@@ -0,0 +1,11 @@
+# Welcome to the City
+# https://www.codewars.com/kata/5302d846be2a9189af0001e4
+def say_hello(name, city, state):
+    return f"Hello, {' '.join(name)}! Welcome to {city}, {state}!"
+
+# Testcases
+# say_hello(["John", "Smith"], "Phoenix", "Arizona") - "Hello, John Smith! Welcome to Phoenix, Arizona!"
+# say_hello(["Jane", "Doe"], "New York", "New York") - "Hello, Jane Doe! Welcome to New York, New York!"
+# say_hello(["Bob", "Dylan"], "Los Angeles", "California") - "Hello, Bob Dylan! Welcome to Los Angeles, California!"
+
+# Status - Passed
