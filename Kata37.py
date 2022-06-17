e57fed61e22b0f1680a240173ffae13f2ff8c148
Alperen Aga <alperencode@gmail.com>
2022-06-17 16:56:35 +0300

    Add Kata37

---
+++ Kata37.py
@@ -0,0 +1,13 @@
+# Stop gninnipS My sdroW!
+# https://www.codewars.com/kata/5264d2b162488dc400000001
+def spin_words(sentence):
+    return ' '.join([word[::-1] if len(word)>=5 else word for word in sentence.split(" ")])
+
+# Testcases
+# spin_words("Welcome") - "emocleW"
+# spin_words("to") - "to"
+# spin_words("CodeWars") - "sraWedoC"
+# spin_words("Hey fellow warriors") - "Hey wollef sroirraw"
+# spin_words("This sentence is a sentence") - "This ecnetnes is a ecnetnes"
+
+# Status - Passed - 460ms
