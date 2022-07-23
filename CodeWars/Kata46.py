bc6a86734f129d3f5785c0cf8ee1e9a2bf73182e
Alperen Aga <alperencode@gmail.com>
2022-07-23 04:49:36 +0300

    Add Kata46

---
+++ CodeWars/Kata46.py
@@ -0,0 +1,11 @@
+# Reverse list
+# https://www.codewars.com/kata/57a04da9e298a7ee43000111/
+def reverse_list(lst):
+    return lst[::-1]
+
+# Testcases
+# ([], []),
+# ([1,2,3,4], [4,3,2,1]),
+# ([2,4,5,6], [6,5,4,2]),
+
+# Status - Passed
