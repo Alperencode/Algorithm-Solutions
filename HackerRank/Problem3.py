89cacce5e12f12e5fdcdfe8fb7f552391c6348f3
Alperen Aga <alperencode@gmail.com>
2022-08-29 00:50:39 +0300

    Add HackerRank - Problem3 and Problem4

---
+++ HackerRank/Problem3.py
@@ -0,0 +1,16 @@
+# Plus Minus
+# https://www.hackerrank.com/challenges/plus-minus/
+def plusMinus(arr):
+    p,n,z = 0,0,0
+    for i in arr:
+        if i>0: p+=1
+        elif i==0: z+=1
+        else: n+=1
+    
+    print(f"{p/len(arr)}\n{n/len(arr)}\n{z/len(arr)}")
+
+# Testcases
+# -4 3 -9 0 4 1 - 0.500000 0.333333 0.166667
+# 1 2 3 -1 -2 -3 0 0 - 0.375000 0.375000 0.250000
+
+# Status - Passed
