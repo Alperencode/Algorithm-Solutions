166241ae3d51b4137088a4fe7282a841b176cfd8
Alperen Aga <alperencode@gmail.com>
2022-09-01 11:14:20 +0300

    Add Grading-Students

---
+++ HackerRank/Grading-Students.py
@@ -0,0 +1,13 @@
+# Grading Students
+# https://www.hackerrank.com/challenges/grading/problem
+
+def gradingStudents(grades):
+    for index,grade in enumerate(grades):
+        if grade%5 >= 3 and grade >= 38:
+            grades[index] += 5-grade%5
+    return grades
+
+# Testcases
+# 73 67 38 33 - 75 67 40 33
+
+# Status - Passed 
