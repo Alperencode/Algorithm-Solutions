19edc1c53fc2b3d76c0a99a4df9c04212d44ea60
Alperen Aga <alperencode@gmail.com>
2022-09-02 17:46:20 +0300

    Add Flipping-the-Matrix

---
+++ HackerRank/Flipping-the-Matrix.py
@@ -0,0 +1,14 @@
+# Flipping the Matrix
+# https://www.hackerrank.com/challenges/flipping-the-matrix/problem
+
+def flippingMatrix(matrix):
+    n = len(matrix)//2
+    ans = 0
+    for i in range(n):
+        for j in range(n):
+            ans += max(matrix[i][j],matrix[i][2*n-1-j],matrix[2*n-1-i][j],matrix[2*n-1-i][2*n-1-j])
+    return ans
+
+##Note: I'll document this solution
+
+# Status: Accepted
