d1104703e14e20229fe21d9e3cac08bef38a13ea
Alperen Aga <alperencode@gmail.com>
2023-04-01 08:48:10 +0300

    Initialize AlgoLeague Add Solve-This-First

---
+++ AlgoLeague/Solve-This-First.c
@@ -0,0 +1,13 @@
+/* https://algoleague.com/problem/solve-this-first/detail */
+#include <stdio.h>
+
+int main() {
+	// write your code here
+	int a,b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a+b);
+
+	return 0;
+}
+
+// Status: Accepted
