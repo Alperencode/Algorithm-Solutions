7fff529da0c442b57d521a6937416053c9424d5d
Alperen Aga <alperencode@gmail.com>
2023-09-30 12:34:22 +0300

    Add LeetCode/Same-Tree-u

---
+++ LeetCode/Same-Tree-u.py
@@ -0,0 +1,43 @@
+# 100. Same Tree
+# https://leetcode.com/problems/same-tree/description/?source=submission-noac
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def traverse(self, node, arr):
+        # Postorder traverse
+        if node is None:
+            # Need to find another way than appending None
+            arr.append(node)
+            return
+
+        self.traverse(node.left, arr)
+        self.traverse(node.right, arr)
+        arr.append(node.val)
+        return
+
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        arr1, arr2 = list(), list()
+        self.traverse(p, arr1)
+        self.traverse(q, arr2)
+        for index, _ in enumerate(arr1):
+            if arr1[index] != arr2[index]:
+                return False
+        return True
+
+# Testcases
+# p = [1,2,3], q = [1,2,3], true
+# p = [1,2], q = [1,null,2], false
+# p = [1,2,1], q = [1,1,2], false
+
+# Status - Wrong Answer
